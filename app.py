e51695386220fcf8141aa28ab317219b0e15841c
root <root@localhost.localdomain>
2017-10-25 13:23:00 -0400

    added bot

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask
+import os
+import asyncio
+import discord
+from discord.ext import commands
 
 app = Flask(__name__)
 @app.route('/')
@@ -7,3 +11,20 @@ def index():
 
 if __name__ == "__main__":
    app.run()
+bot = commands.Bot(command_prefix='prefix', description='Chatterbox')
+
+@bot.event
+async def on_message(message):
+    mesString = message.content
+    fileToWrite = open("transfer.txt", "w")
+    mesStringFinal = mesString[8:]
+    fileToWrite.write(mesStringFinal)
+    fileToWrite.close()
+    os.system("python2 readToFile.py")
+
+    fileToOpen = open("transfer.txt").read()
+    text = fileToOpen
+    channel = message.channel
+    if message.content.startswith("!cgsbot"): await bot.send_message(channel, text)
+
+bot.run('MzYzNDY2Njg2MTMwODE0OTg3.DNEpwg.J9xWSLkaiqOerH1CiZDQgu7ktYA')
